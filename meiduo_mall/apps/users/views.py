3b6b70aed273c70c8744508b2d8f34215d0e4551
leftzhf <acemagic123@outlook.com>
2022-05-06 14:19:36 +0800

    用户名重复检测

--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
+class UsernameCountView(View):
+
+    def get(self,request,username):
+        """
+        验证用户名是否重复
+        Args:
+            request: 请求对象
+            username: 用户名
+
+        Returns:
+        没有重复返回0，有重复返回1.
+        """
+        count=User.objects.filter(username=username).count()
+        return JsonResponse({'code':0,'count':count,'errmsg':'ok'})
